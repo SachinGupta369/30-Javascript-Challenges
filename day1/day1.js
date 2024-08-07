00c843eed3e9fa34641da05138108dc547029f0f
Sachin Gupta <sachingupta0098@gmail.com>
2024-08-07 19:49:33 +0530

    day1 and day2 files

---
+++ day1/day1.js
@@ -0,0 +1,41 @@
+// Task 1 Variable Declaration
+var num = 232;
+console.log(num); // 232
+
+let data = 'hello'
+console.log(data); // hello
+
+//task 2 constant declaration
+const bool = true
+const bool1 = false
+console.log(bool);
+console.log(bool1);
+
+// task3 Data types
+console.log(typeof num); // number
+console.log(typeof data); //string
+console.log(typeof bool1); // boolean
+const myarr = [0,24,'Jai Shri Ram', "Narayan"]
+console.log(typeof myarr);
+
+Fun =  () =>{console.log("hee");}
+
+console.log(typeof Fun(),`function`);
+
+//task 4 Reassigning Variable
+
+let name1 = "Narayan"
+console.log(name1);
+name1 = "Jai Shri Ram"
+console.log(name1);
+
+//task 5 understanding const
+
+const pi = 3.14
+console.log(pi);
+pi = 4
+console.log(pi);
+
+
+
+
---
+++ notes.md
@@ -0,0 +1,11 @@
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/SachinGupta369/30-Javascript-Challenges.git
+#git push -u origin main
+
+<!-- Day 1 const, let & var -->
+just look day1.js for review
+
+<!-- Day 2  -->
