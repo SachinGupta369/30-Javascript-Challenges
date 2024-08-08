c8e62f6d4e4dbc0eb1baca04eaccb3d64c715c6e
Sachin Gupta <sachingupta0098@gmail.com>
2024-08-08 12:53:57 +0530

    main

--- day2/day2.js
+++ day2/day2.js
@@ -1,1 +1,123 @@
-// Task 1
+// Task/Activties 1
+function addTwoNumber(num1,num2){
+    add = num1+num2
+    console.log(`Add Two Number`,add);
+}
+
+addTwoNumber(12,3)
+
+//task 2
+
+substract = (num1,num2) => { console.log(`Substract Two Number`,num1-num2);}
+substract(13,3)
+
+//task 3
+
+multiply = (num1,num2) => {console.log(`Multiply`,num1*num2);}
+multiply(3,3)
+
+//task 4
+
+divide = (num1,num2) => {console.log(`Divide`,num1/num2)}
+divide(4,2)
+
+// Task 5
+// To find the reminder 
+
+toFindReminder = (num1,num2)=>{ console.log(`Reminder of ${num1} & ${num2}`,num1%num2); }
+toFindReminder (10,2)
+toFindReminder (10,3)
+
+// Task 6 Assignment Operator
+
+let scoresAdd = 1
+scoreAdd=()=>{ scoresAdd +=2; console.log(`+= Assignment Operator`,scoresAdd);}
+scoreAdd()
+
+// Task 7 Assignment Operator
+
+let scoresSub = 3
+scoreSub = ()=>{ scoresSub -=2; console.log(`-= Assignment Operator`,scoresSub); }
+scoreSub()
+
+// Activities 3 Comparsion Operator
+// Task 8 
+
+greaterThan=(num1,num2)=>{ greater=num1>num2; 
+    console.log(`${num1} is Greater than ${num2} :`,greater);
+}
+greaterThan(12,3)
+greaterThan(2,3)
+
+lesserThan=(num1,num2)=>{lesser=num1<num2; 
+    console.log(`${num1} is Lesser than ${num2} :`,lesser);
+}
+lesserThan(5,8)
+lesserThan(10,8)
+
+//Task 9
+
+greaterEqualTo=(num1,num2)=>{ result= num1>=num2; 
+    console.log(`${num1} is Greater equal to ${num2} :`, result);
+ }
+greaterEqualTo(12,12)
+greaterEqualTo(12,10)
+greaterEqualTo(2,10)
+
+lesserEqualTo=(num1,num2)=>{ result= num1<=num2; 
+    console.log(`${num1} is lesser equal to ${num2} :`, result);
+ }
+lesserEqualTo(12,12)
+lesserEqualTo(12,10)
+lesserEqualTo(2,10)
+
+// Task 10 
+
+console.log(`comparison with "=="`,undefined==0);
+console.log(`comparison with "=="`,undefined===0);
+// console.log(undefined>=0);
+// console.log(undefined<=0);
+// console.log(undefined<0);
+// console.log(undefined>0);
+
+// console.log(null<=0);
+// console.log(null ===0);
+
+// Activity 4 Logical Operator
+// Task 11  
+
+let isLoggedOut = false;
+let isLoggedIn = true;
+
+console.log( isLoggedOut && `No Login`);
+console.log( isLoggedIn && `you are a Senior Web Developer`);
+
+// Task 12 
+
+function greet(name){
+    let greeting = name || "Jai Shri ram" ;
+    console.log("Ram Ram",greeting); 
+}
+
+greet("Narayan")
+greet("")
+greet()
+
+//Task 13 
+
+let username = true
+
+if(!username == true){
+    console.log(`!username is false`,!username);
+}else{
+    console.log(`!username is true`,!username);
+    
+}
+
+// Activity 5 Ternary Operator
+// Task 14
+
+let checkNum = '1'
+
+let resultess = checkNum === 1 ? "Number 1 is the pure Number" : "Number 1 is manipulated number" 
+console.log(resultess);
--- notes.md
+++ notes.md
@@ -9,3 +9,34 @@
 just look day1.js for review
 
 <!-- Day 2  -->
+
+divide method "/"
+
+4/2 = 2;
+2/2 = 1;
+5/2 = 2.5;
+
+To Find Remainder we use modulus "%"
+10 is Dividend and 3 is divisor 
+10 % 3 remainder is 1
+
+same goes to 10 is dividend and 2 is divisor
+10 % 2 remainder is 0
+
+# And Operator 
+( True && "Execute" ) O/p will be "Execute"
+( False &&  "Execute") o/p will be False
+
+# OR Operator 
+OR Operator is the operator in which anyone expression should be true to execute or else return False
+
+(Expression 1 || Expression 2) // if expression 1 is true then expression2 don`t execute
+(Expression 1 || Expression 2) // if expression 1 is false then expression2 will be execute
+
+# Ternary Operator
+Ternary Operator is a condition operator where ( Condition ? Express1 : Express2) if condition is true then Express1 will execute if condition is false then Express2 will be execute
+
+(condition ? Express1 : Express2)
+i.e. let result = (condition ? Express1 : Express2)
+console.log(result)
+
